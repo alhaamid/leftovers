e6c08521641bf55e89e9d1083f269d0017af51f1
Haamid <haamid@yelp.com>
2019-07-24 14:51:57 -0700

    implemented the rest of user functions

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -4,10 +4,58 @@ import promise from "promise";
 
 export class UserController {
     User: mongoose.Model<mongoose.Document> = mongoose.model('User', UserSchema);
+    userKeysToHide: string[] = ['password'];
 
     constructor() {}
 
-    public getAUser(handle: string) {
+    public addNewUser(userJSON) {
+        let newUser = new this.User(userJSON);
+        return new promise <Result>((resolve, reject) => {
+            newUser.save((err, user) => {
+                if (err) {
+                    console.log("Error while creating a new user", err);
+                    reject({code: 500, result: err});
+                } else {
+                    resolve({code: 201, result: user});
+                }
+            });
+        });
+    }
+
+    public checkCredentials(credentials) {
+        const handle = credentials.handle;
+        const password = credentials.password;
+        return new promise<Result> ((resolve, reject) => {
+            this.getAUser(handle, false)
+            .then(obj => {
+                const user = obj["result"];
+                if (user["password"]==password) {
+                    const veiledUser = this.removeSomeKeys(user, this.userKeysToHide);
+                    resolve({code: 200, result: veiledUser});
+                } else {
+                    resolve({code: 404, result: "Credentials are incorrect"});
+                }
+            })
+            .catch(obj => obj);
+        });
+    }
+
+    public getAllUsers() {
+        return new promise <Result> ((resolve, reject) => {
+            this.User.find((err: any, users: any) => {
+                if (err) {
+                    console.log(err);
+                    reject({code: 404, result: err})
+                } else {
+                    resolve({code: 200, result: users});
+                }
+            }, (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    public getAUser(handle: string, veil: boolean) {
         return new promise<Result>((resolve, reject) => {
             let condition = { handle: { $eq: handle } };
             this.User.findOne(condition, (err, user) => {
@@ -16,7 +64,13 @@ export class UserController {
                     reject({code: 500, result: err});
                 } else {
                     if (user) {
-                        resolve({code: 200, result: user});
+                        let result = undefined;
+                        if (veil) {
+                            result = this.removeSomeKeys(user, this.userKeysToHide);
+                        } else {
+                            result = user;
+                        }
+                        resolve({code: 200, result: result});
                         // user.populate('listings', (err, populatedUser: mongoose.Document) => {
                         //     if (populatedUser) {
                         //         resolve({code: 200, result: populatedUser});
@@ -34,33 +88,15 @@ export class UserController {
         });
     }
 
-    public addNewUser(userJSON) {
-        let newUser = new this.User(userJSON);
-        return new promise <Result>((resolve, reject) => {
-            newUser.save((err, user) => {
-                if (err) {
-                    console.log("Error while creating a new user", err);
-                    reject({code: 500, result: err});
-                } else {
-                    resolve({code: 201, result: user});
-                }
-            });
+    private removeSomeKeys(userSchemaJSON, keepWhichKeys: string[]) {
+        let newObj = JSON.parse(JSON.stringify(userSchemaJSON));
+        let allKeys = Object.keys(newObj);
+        allKeys.forEach((key) => {
+            if (keepWhichKeys.indexOf(key) != -1) {
+                delete newObj[key];
+            }
         });
-    }
-
-    public getAllUsers() {
-        return new promise <Result> ((resolve, reject) => {
-            this.User.find((err: any, users: any) => {
-                if (err) {
-                    console.log(err);
-                    reject({code: 404, result: err})
-                } else {
-                    resolve({code: 200, result: users});
-                }
-            }, (err) => {
-                console.log(err);
-            });
-        });
+        return newObj;
     }
 }
 
--- backend/routes.ts
+++ backend/routes.ts
@@ -7,22 +7,27 @@ let userController = new UserController();
 
 routes.route('/')
 .get((req, res) => {
-    // res.send('Backend Server is working');
-    userController.getAllUsers()
+    res.send('Backend Server is working');
+});
+
+routes.route('/login')
+.put((req, res) => {
+    userController.checkCredentials(req.body)
     .then(obj => {res.status(obj["code"]).send(obj["result"]);})
     .catch(obj => {res.status(obj["code"]).send(obj["result"]);})
+})
+
+routes.route('/user')
+.post((req, res) => {
+    userController.addNewUser(req.body)
+    .then(obj => {res.status(obj["code"]).send(obj["result"]);})
+    .catch(obj => {res.status(obj["code"]).send(obj["result"]);});
 });
 
 routes.route('/user/:handle')
 .get((req, res) => {
     const handle = req.params["handle"];
-    userController.getAUser(handle)
+    userController.getAUser(handle, true)
     .then(obj => {res.status(obj["code"]).send(obj["result"]);})
     .catch(obj => {res.status(obj["code"]).send(obj["result"]);})
-})
-
-.post((req, res) => {
-    userController.addNewUser(req.body)
-    .then(obj => {res.status(obj["code"]).send(obj["result"]);})
-    .catch(obj => {res.status(obj["code"]).send(obj["result"]);});
 });
