984d21053eb1bbc5d0df5b4c1f6d6ec3c279e179
Haamid <haamid@yelp.com>
2019-07-25 15:37:23 -0700

    sending controller

---
+++ frontend/leftovers/src/services/findController.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+class findController {
+    constructor () {
+        this.backendUrl = 'http://localhost:4201';
+        // 'https://jsonplaceholder.typicode.com/users'
+    }
+
+    getAvailableListings() {
+        const url = `${this.backendUrl}/listing`;
+        return new Promise ((resolve, reject) => {
+            axios.get(url)
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(rej => reject(rej))
+        })
+    }
+
+    search () {
+        return [
+            {
+              imageUrl: "search",
+              location: "search",
+              title: "search",
+              description: "search",
+            }
+        ]
+    }
+}
+
+export default findController;
+
+// [
+//     {
+//       imageUrl: "airpods.jpeg",
+//       location: "8F",
+//       title: "airpods",
+//       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.",
+//     },
+//     {
+//       imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51z376z5iBL._SL1200_.jpg",
+//       location: "8F",
+//       title: "airpods",
+//       description: "These are MY headphones!",
+//     },
+// ]
