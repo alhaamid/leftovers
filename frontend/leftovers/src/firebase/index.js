563bae5274ea13daa413938e900a711f4d469f0b
Abdul Lateef Haamid <abdullateefhaamid@gmail.com>
2019-07-28 12:09:10 -0700

    removed sensitive credentials

--- frontend/leftovers/src/firebase/index.js
+++ frontend/leftovers/src/firebase/index.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD9DjHrHjD2EDkJa19CxoKV6s72PGWmk2w",
-    authDomain: "lnf-learning.firebaseapp.com",
-    databaseURL: "https://lnf-learning.firebaseio.com",
-    projectId: "lnf-learning",
-    storageBucket: "lnf-learning.appspot.com",
-    messagingSenderId: "488419640521",
-    appId: "1:488419640521:web:595b95bcb6a3a2e0"
+    
 };
 
 // Initialize Firebase
